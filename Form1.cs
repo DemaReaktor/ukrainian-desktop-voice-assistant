0e01f0e670fb9f54d28954969ae624049fc5f6cf
Dema <67798322+DemaReaktor@users.noreply.github.com>
2022-12-16 13:05:57 +0200

    ukrainian

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using System.Speech.Synthesis;
 using System.Xml.Linq;
 using System.Linq;
 using Vosk;
 using NAudio.Wave;
+using System.Collections.Generic;
+using mphdict.Models.morph;
+using mphdict;
+using System.Data.Entity;
+using System.Data.Entity.Core;
 
 namespace Speech
 {
@@ -29,6 +35,8 @@ namespace Speech
 
         public Form1()
         {
+            var db = new mphDb();
+            db.getContext().words_list;
             InitializeComponent();
             voskRecognizer = new VoskRecognizer(new Model("E:\\Инет\\vosk-model-uk-v3\\vosk-model-uk-v3"), 44100f);
 
@@ -37,99 +45,23 @@ namespace Speech
             waveIn.DataAvailable += WaveInOnDataAvailable;
             waveIn.StartRecording();
 
-            //synthen = new SpeechSynthesizer();
-            //textBox1.Text = "привіт паляниця";
-            //if (File.Exists("Settings.xml"))
-            //{
-            //    var xElement = XElement.Load("Settings.xml");
-            //    rate = int.Parse(xElement.Element("rate").Value);
-            //    volume = int.Parse(xElement.Element("volume").Value);
-            //    voice = xElement.Element("voice").Value;
-            //}
-            //else
-            //{
-            //    rate = 1;
-            //    volume = 50;
-            //    voice = "Anatol";
-            //    SaveSettings();
-            //}
+            synthen = new SpeechSynthesizer();
+            textBox1.Text = "привіт паляниця";
+            if (File.Exists("Settings.xml"))
+            {
+                var xElement = XElement.Load("Settings.xml");
+                rate = int.Parse(xElement.Element("rate").Value);
+                volume = int.Parse(xElement.Element("volume").Value);
+                voice = xElement.Element("voice").Value;
+            }
+            else
+            {
+                rate = 1;
+                volume = 5;
+                voice = "Anatol";
+                SaveSettings();
+            }
         }
-
-        //void OnAudio(object sender, SpeechRecognizedEventArgs e)
-        //{
-        //    LinkedList<string> list;
-        //    int result;
-
-        //    switch (e.Result.Grammar.Name)
-        //    {
-        //        case "general":
-        //            Speak("до зустрічі");
-        //            Application.Exit();
-        //            break;
-        //        case "voice":
-        //            list = new LinkedList<string>();
-        //            foreach (var element in e.Result.Words.ToArray())
-        //                list.AddLast(element.Text);
-        //            list.RemoveFirst();
-        //            bool isVolume = list.First.Value == "volume";
-        //            list.RemoveFirst();
-
-        //            if (isVolume && TryGetValue(list, out result, 0, 100))
-        //            {
-        //                volume = result;                                                      
-        //                SaveSettings();
-        //                Speak("гучність " + volume);
-        //            }
-        //            if (!isVolume && TryGetValue(list, out result, -10, 10))
-        //            {
-        //                rate = result;
-        //                SaveSettings();
-        //                Speak("швидкість " + rate);
-        //            }
-        //            break;
-        //        case "write":
-        //            long? value = null;
-        //            label2.Text = "";
-        //            foreach (var element in e.Result.Words)
-        //                if (WordsToNumbers.TryConvertToNumbers(element.Text, out long longg))
-        //                {
-        //                    if (value is null)
-        //                        value = longg;
-        //                    else
-        //                        value += longg;
-        //                }
-        //                else
-        //                {
-        //                    if (value != null)
-        //                        label2.Text += value.ToString() + " ";
-        //                    label2.Text += element.Text + " ";
-        //                    value = null;
-        //                }
-        //            if (value != null)
-        //                label2.Text += value.ToString() + " ";
-        //            AutoItX.Send(label2.Text.Remove(0,5));
-        //            break;
-        //        case "copy":
-        //            //Clipboard.SetText();
-        //            //Speak("скопійовано");
-        //            Speak("не скопійовано, команда ще не готова");
-        //            break;
-        //        case "paste":
-        //            AutoItX.Send(Clipboard.GetText());
-        //            Speak("вставлено");
-        //            break;
-        //        case "showVoices":
-        //            label2.Text = "";
-        //            foreach (var element in new SpeechSynthesizer().GetInstalledVoices())
-        //                label3.Text += element.VoiceInfo.Culture + ", " + element.VoiceInfo.Gender + ", " + element.VoiceInfo.Name + "\n";
-        //            Speak("ось список");
-        //            break;
-        //        case "setVoice":
-        //            voice = e.Result.Text.Remove(0,10);
-        //            Speak("тепер я "+ voice);
-        //            break;
-        //    }
-        //}
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -139,7 +71,7 @@ namespace Speech
         private Prompt Speak(string text)
         {
             synthen.Rate = rate;
-            synthen.Volume = volume;
+            synthen.Volume = volume*10;
             synthen.SetOutputToDefaultAudioDevice();
 
             var builder = new PromptBuilder();
@@ -168,18 +100,59 @@ namespace Speech
             {
                 var text1 = text.Remove(0, 14);
                 label3.Text = text1.Remove(text1.Length - 3, 3);
-                Command(text);
+                Command(label3.Text);
             }
         }
 
         void Command(string text) 
         {
-            switch (text)
+            if (synthen.State != SynthesizerState.Speaking)
             {
-                case "вийти":
-                    Application.Exit();
-                    break;
+                switch (text)
+                {
+                    case "вийти":
+                        Application.Exit();
+                        break;
+                }
+
+                if (text.Split(' ')[0] == "скажи")
+                    Speak(text.Remove(0, 6));
+                else
+                    if(text.Split(' ')[0] == "гучність")
+                {
+                    volume = WordsToInt.ToInt(text.Remove(0,9));
+                    SaveSettings();
+                    Speak(volume.ToString() + ", тепер ти мене чуєш?");
+                }
+                else
+                    if (text.Split(' ')[0] == "швидкість" && text.Length>10)
+                {
+                    rate = WordsToInt.ToInt(text.Remove(0, 10));
+                    SaveSettings();
+                    Speak(rate.ToString() + ", шла Саша по шосе і сосала сушку");
+                }
+
+                text = null;
             }
+        }
+    }
+    class WordsToInt
+    {
+        static readonly Dictionary<string, int> words = new Dictionary<string, int>() {
+            { "нуль",0},{ "один",1},{ "два",2},{ "три",3},{ "чотири",4},{ "п'ять",5},{ "шість",6},{ "сім",7},{ "вісім",8},{ "дев'ять",9},{ "десять",10} };
+        public static int ToInt(string text)
+        {
+            int result = 0;
+            bool isNegative = false;
+            foreach (var e in text.Split(' '))
+            {
+                if (words.ContainsKey(e))
+                    result += words[e];
+
+                if (e == "мінус")
+                    isNegative = true;
+            }
+            return isNegative? -result: result;
         }
     }
 }
